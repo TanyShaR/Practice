0321584ca4ce421ae6efcaf2ef1046b0008e238f
TanjaRetter <88773218+TanyShaR@users.noreply.github.com>
2025-08-11 14:55:16 +0200

    update

--- React/Lesson5/src/App.jsx
+++ React/Lesson5/src/App.jsx
@@ -1,11 +1,9 @@
-import { NavLink, Route, Routes } from "react-router"
+import { Link, NavLink, Route, Routes } from "react-router"
 import Home from "./pages/Home/Home";
 import Blog from "./pages/Blog/Blog";
 import BlogDetails from "./pages/BlogDetails/BlogDetails";
 import Contact from "./pages/Contact/Contact";
 import About from "./pages/About/About";
-import Users from "./pages/Users/Users";
-import UserDetails from "./pages/UserDetails/UserDetails";
 
 const setActiveLink = ({isActive}) => isActive ? "navbar__item navbar__item-active" : "navbar__item";
 
@@ -18,7 +16,6 @@ const App = () => {
             <li><NavLink to="/blog" className={setActiveLink}>Blog</NavLink></li>
             <li><NavLink to="/contact" className={setActiveLink}>Contact</NavLink></li>
             <li><NavLink to="/about" className={setActiveLink}>About</NavLink></li>
-            <li><NavLink to="/users" className={setActiveLink}>Users</NavLink></li>
           </ul>
         </nav>
 
@@ -29,9 +26,6 @@ const App = () => {
               <Route path="/blog/:id" element={<BlogDetails />}/>
               <Route path="/contact" element={<Contact />}/>
               <Route path="/about" element={<About />}/>
-              <Route path="/users" element={<Users />}/>
-              <Route path="/users/:userId" element={<UserDetails />}/>
-              <Route path="*" element={<h1> Not Found </h1>}/>
            </Routes>
         </main>
     </>
